8320ac24175db56cdbf8baf9822860d2d8074ede
NotBlizzard <furryblizzard@gmail.com>
2020-06-09 20:08:07 -0400

    update settings.py.

--- lark/settings.py
+++ lark/settings.py
@@ -140,7 +140,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
